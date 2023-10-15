edb69534e690a686571a897bdd2aa3a32e14cda8
SOURAV MAJEE <122084016+Sourav2226@users.noreply.github.com>
2023-10-15 13:04:09 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+var speed=new typed(
+    ".text",{
+        String:["Frontend Devloper","YouTuber","Web Devloper"],
+        typespeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    }
+);
+
